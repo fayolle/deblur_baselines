704c04de8529cbec098a821956e37d5ae4e379ee
fayolle <pafayolle@gmail.com>
2025-04-08 11:10:48 +0900

    Add comparisons to the implementation in MATLAB image processing toolbox

---
+++ test_improc_toolbox.m
@@ -0,0 +1,63 @@
+% Script to compare the implementation of Wiener and RL to the
+% implementations in MATLAB image processing toolbox. 
+% This requires the image processing toolbox to be installed. 
+%
+
+close all;
+clear;
+clc;
+
+addpath('./images/');
+addpath('./kernels/');
+
+
+% Barbara 
+xin = im2double(imread('barbara_face.png'));
+
+% eccv3 kernel
+k = im2double(imread('eccv3_blur_kernel.png'));
+k = k./sum(k(:));
+K = psf2otf(k,size(xin));
+f = @(x) real(ifft2(fft2(x).*K));
+
+% Observed blurred image
+yout = f(xin);
+figure, imshow([xin yout]), title('Input/Blurred');
+
+max_iter = 100; 
+
+% Wiener 
+w_out = Wiener(K, yout);
+w_out_t = deconvwnr(yout, k);
+
+% RL 
+rl_out = RL(K, yout, max_iter);
+rl_out_t = deconvlucy(yout, k, max_iter);
+
+% Display the results 
+figure, imshow([w_out w_out_t]), title('Wiener/Toolbox');
+figure, imshow([rl_out rl_out_t]), title('RL/Toolbox');
+
+
+% With noise 
+noise_mean = 0.0;
+noise_var = 0.00001;
+F = @(x) imnoise(f(x), 'gaussian', noise_mean, noise_var);
+
+% Observed blurred image
+yout = F(xin);
+figure, imshow([xin yout]), title('Input/Blurred and noisy');
+
+max_iter = 100; 
+
+% Wiener 
+w_out = Wiener(K, yout);
+w_out_t = deconvwnr(yout, k);
+
+% RL 
+rl_out = RL(K, yout, max_iter);
+rl_out_t = deconvlucy(yout, k, max_iter);
+
+% Display the results 
+figure, imshow([w_out w_out_t]), title('Wiener/Toolbox');
+figure, imshow([rl_out rl_out_t]), title('RL/Toolbox');
